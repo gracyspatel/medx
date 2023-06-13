e2d744c06acfe3eb7e1a38a5bf5c3c4ea604a660
Ayush Patel <ayush.patel@1Rintern.local>
2023-06-13 15:31:51 +0530

    model

--- base/models.py
+++ base/models.py
@@ -14,21 +14,31 @@ CHOICES = (
 
 
 # Create your models here.
-class patient_details(models.Model):
-    p_id = models.IntegerField()
-    p_name = models.CharField(max_length=100)
-    p_email = models.EmailField(max_length=100)
-    p_phone_number = models.CharField(max_length=10, null=False)
-    p_DOB = models.DateField(max_length=8)
-    p_address = models.CharField(max_length=100)
-    p_sex = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    p_blood_group = models.CharField(max_length=10)
-    p_weight = models.IntegerField()
-    p_height = models.IntegerField()
-    p_previous_medical_history = models.CharField(max_length=100)
-    p_smoking = models.CharField(max_length=3, choices=CHOICES)
-    p_alcohol = models.CharField(max_length=3, choices=CHOICES)
-    p_drugs = models.CharField(max_length=3, choices=CHOICES)
+class Patient(models.Model):
+    patient_id = models.IntegerField()
+    patient_name = models.CharField(max_length=100)
+    patient_email = models.EmailField(max_length=100)
+    patient_phone_number = models.CharField(max_length=10, null=False)
+    patient_dob = models.DateField(max_length=8)
+    patient_address = models.CharField(max_length=100)
+    patient_sex = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    patient_blood_group = models.CharField(max_length=10)
+    patient_weight = models.IntegerField()
+    patient_height = models.IntegerField()
+    patient_previous_medical_history = models.CharField(max_length=100)
+    patient_smoking = models.CharField(max_length=3, choices=CHOICES)
+    patient_alcohol = models.CharField(max_length=3, choices=CHOICES)
+    patient_drugs = models.CharField(max_length=3, choices=CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.patient_name
+
+
+# class Doctor_details(models.Model):
+#     d_id = models.IntegerField()
+#     d_name = models.CharField(max_length=100)
+#     d_email = models.EmailField(max_length=100)
+#     d_password = models.CharField
 
